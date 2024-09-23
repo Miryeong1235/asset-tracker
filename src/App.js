5a69dc0d19095896a06674406d53d73ef07b17c4
Misuzu <me.tpn23@gmail.com>
2024-09-23 16:40:01 -0700

    Change colour

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './inu-test.JPG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="test" />
+        <p>
+          Hi this is test. <br />
+          This is an app to track your assets.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
